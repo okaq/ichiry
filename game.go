82ded5bb11c6d092dd87ac4e67758457a0faaf5d
aq <aq@okaq.com>
2020-12-06 09:29:45 -0500

    Setup config file

---
+++ game.go
@@ -0,0 +1,23 @@
+// file coordination
+// <aq@okaq.com>
+// 2020-12-06
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/util"
+	"os"
+)
+
+const (
+	CONFIG = "config.js"
+)
+
+func main() {
+	// parse json
+}
+
+
