7af6f247c9c3a5c3daa1be8f75d0e9e27b8f83c9
aq <aq@okaq.com>
2020-12-07 10:52:11 -0500

    Games input data format

--- game.go
+++ game.go
@@ -16,8 +16,32 @@ const (
 	CONFIG = "config.js"
 )
 
+// type def for config input
+type Games struct {
+	console string `json:console`
+	dir string `json:dir`
+}
+
+func (g Games) String() string {
+	b0, err := json.Marshal(g)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(b0)
+}
+
+func load() {
+	ioutil.ReadAll(CONFIG)
+}
+
+func parse() {
+
+}
+
 func main() {
 	// parse json
+	load()
+	parse()
 }
 
 
